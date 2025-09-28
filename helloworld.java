683594f74a0315e262a67fcc715fba88e575d16f
Sreerag Krishna <sreerag2114@gmail.com>
2025-09-28 18:57:09 +0530

    Upto PART 10

---
+++ helloworld.java
@@ -0,0 +1,15 @@
+
+public class helloworld {
+
+    public static void main(String arg[]) {
+        System.out.println("Hello world");
+        int a = 10;
+        long b = 12345l;
+        short c = 2;
+        byte d = 2;
+        float e = 45.4f;
+        double f = 455.2342;
+        char g = 's';
+        boolean h = true;
+    }
+}
